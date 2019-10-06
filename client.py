a95a6af33732f22dcbfed96a39f092895dff017b
Fettes <396511673@qq.com>
2019-10-05 23:33:04 -0400

    x

--- client.py
+++ client.py
@@ -58,7 +58,7 @@ class EchoClient(asyncio.Protocol):
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
-    coro = playground.create_connection(EchoClient, '20194.0.1.1', 8866)
+    coro = playground.create_connection(EchoClient, '20194.0.1.1', 8886)
 
     # loop.set_debug(enabled=True)
     # from playground.common.logging import EnablePresetLogging, PRESET_DEBUG
