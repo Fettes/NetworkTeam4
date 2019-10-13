1fc9e88df36b6914b1bee7900c5bdf2737b6b234
Fettes <396511673@qq.com>
2019-10-13 15:22:57 -0400

    handshake

--- Handshake/passthrough.py
+++ Handshake/passthrough.py
@@ -2,6 +2,7 @@ from playground.network.packet import PacketType
 from playground.network.packet.fieldtypes import STRING, UINT8, BUFFER, UINT16, BOOL
 from playground.network.packet.fieldtypes.attributes import Optional
 
+
 class HandshakePacket(PacketType):
     DEFINITION_IDENTIFIER = "handshakepacket"
     DEFINITION_VERSION = "1.0"
@@ -10,8 +11,8 @@ class HandshakePacket(PacketType):
     SUCCESS = 1
     ERROR = 2
     FIELDS = [
-        ("SYN",UINT8({Optional:True})),
-        ("ACK",UINT8({Optional:True})),
+        ("SYN", UINT8({Optional: True})),
+        ("ACK", UINT8({Optional: True})),
         ("status", UINT8),
-        ("error",STRING({Optional:True}))
+        ("error", STRING({Optional: True}))
     ]
