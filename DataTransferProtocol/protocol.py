89ebdf08e6d8fd06deb1d8bcfca8c20d569bbf54
Fettes <fengtianshi@hotmail.com>
2019-11-15 21:31:40 -0500

    x

--- DataTransferProtocol/protocol.py
+++ DataTransferProtocol/protocol.py
@@ -479,7 +479,7 @@ class POOP(StackingProtocol):
         self.recv_queue.sort(key=lambda pkt_: pkt_.seq)
         print(isinstance(pkt.data, bytes))
         while self.recv_queue:
-            print("send to application")
+            print(self.recv_queue[0].data)
             if self.recv_queue[0].seq == self.recv_next:
                 self.higherProtocol().data_received(
                     self.recv_queue.pop(0).data)
